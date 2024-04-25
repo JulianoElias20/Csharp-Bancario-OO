1ba5619c6f4d90d49d39b2cd820046b1339e18fd
Juliano Elias <julianorelias94@hotmail.com>
2024-04-25 18:02:20 -0300

    Simulando depósito bancário e saque

---
+++ DepositoBancarioOO/Bancario.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositoBancarioOO
+{
+    internal class Bancario
+    {
+        public int Nconta {get; private set;}
+        public double Saldo { get; private set; }
+        public string Titular {get; set;}
+        
+        public Bancario() { }
+        public Bancario(int nconta, string titular)
+        {
+            Nconta = nconta;
+            Titular = titular;
+            
+        }
+
+        public Bancario(int nconta, string titular, double saldo): this(nconta,titular)
+        {
+
+            Deposito(saldo);
+        }
+
+        public void Deposito(double valor)
+        {
+             Saldo += valor;
+        }
+
+        public void  Saque(double valor)
+        {
+            Saldo= Saldo-valor-5;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {Nconta}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2")}";
+        }
+
+    }
+}
---
+++ DepositoBancarioOO/Program.cs
@@ -0,0 +1,60 @@
+﻿using DepositoBancarioOO;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (s/n)? ");
+            string veri = Console.ReadLine();
+
+            Bancario dados;
+
+            if (veri == "s" || veri == "S")
+            {
+                Console.WriteLine("Entre com o valor para depósito: ");
+                double saldo = double.Parse(Console.ReadLine());
+
+                dados = new Bancario(numero, nome, saldo);
+            }
+
+            else if (veri == "n" || veri == "N")
+            {
+
+                dados = new Bancario(numero, nome);
+
+            }
+            else
+            {
+                dados = new Bancario();
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(dados.ToString());
+
+            Console.Write("Entre com um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine());
+            dados.Deposito(valor);
+
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine(dados.ToString());
+
+            Console.Write("Entre com um valor para saque: ");
+            valor = double.Parse(Console.ReadLine());
+            dados.Saque(valor);
+
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine(dados.ToString());
+
+
+        }
+    }
+}
